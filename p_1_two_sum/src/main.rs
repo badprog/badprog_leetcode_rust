67a2874e9df882d6e04da67fded68609e287c3bc
Mi-K <badprog@users.noreply.github.com>
2025-08-07 19:48:46 +0200

    Removing debug.

--- p_1_two_sum/src/main.rs
+++ p_1_two_sum/src/main.rs
@@ -3,12 +3,4 @@
 // main
 fn main() {
     println!("Hello, from Badprog and Leetcode project 1 :D");
-    // println!("");
-
-    // let nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    // let target = 14;
-
-    // let result = mika_sum(nums, target);
-
-    // println!("result = {result:#?}");
 }
